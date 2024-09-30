76b675bf868e0885b8bc824efa46b64e87ce037a
zengqin huang <112330973+cuso4huang@users.noreply.github.com>
2024-09-30 11:10:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember(){
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var project = document.getElementById("project");
+    var member = document.getElementById("member");
+    member.style.display = "block";
+    skills.style.display = "none";
+    project.style.display = "none";
+    member.style.display = "none";
+}
